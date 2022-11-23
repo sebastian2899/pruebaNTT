b70aba3a97deaeae88aac51fd8ae86cf3df61111
sebastian.ortegon <85312424+sebastian2899@users.noreply.github.com>
2022-11-22 21:07:39 -0500

    Initial version of pruebaNTT generated by generator-jhipster@7.8.1

---
+++ src/main/java/co/com/pruebantt/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package co.com.pruebantt.repository;
+
+import co.com.pruebantt.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/co/com/pruebantt/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package co.com.pruebantt.service.dto;
